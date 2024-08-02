5ed65d5b0a9aef9acce797aa89972d04b53b8d3e
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-08-03 00:38:01 +0700

    Whatsapp Bot

---
+++ handler/handler.go
@@ -0,0 +1,116 @@
+package handler
+
+import (
+	"context"
+	"github.com/golang/protobuf/proto"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow/types/events"
+	"io"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
+type Handler struct {
+	Client         *whatsmeow.Client
+	eventHandlerID uint32
+}
+
+func (handler *Handler) register() {
+	handler.eventHandlerID = handler.Client.AddEventHandler(handler.EventHandler)
+}
+
+func (handler *Handler) EventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		handler.HandleMessage(v)
+		break
+	}
+}
+
+func (handler *Handler) HandleMessage(e *events.Message) {
+	if e.Info.IsFromMe {
+		return
+	}
+
+	message := e.Message.GetConversation()
+
+	if string(message[0]) != os.Getenv("PREFIX") {
+		return
+	}
+
+	filename := strings.Replace(strings.Split(message, " ")[0], ".", "", 1)
+	text, err := handler.GetTextMessage(filename)
+	if err != nil {
+		handler.SendMessage(e, handler.GetHelpMessage())
+		return
+	}
+
+	handler.SendMessage(e, text)
+}
+
+func (handler *Handler) GetHelpMessage() string {
+	help, err := handler.GetTextMessage("help")
+	if err != nil {
+		panic(err)
+	}
+	return help + handler.ListMessages()
+}
+
+func (handler *Handler) ListMessages() string {
+	workdir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	files, err := os.ReadDir(path.Join(workdir, "messages"))
+	if err != nil {
+		panic(err)
+	}
+
+	text := ""
+	for _, file := range files {
+		text += "\n" + os.Getenv("PREFIX") + file.Name()
+	}
+	return text
+}
+
+func (handler *Handler) SendMessage(e *events.Message, message string) {
+	_, err := handler.Client.SendMessage(context.Background(), e.Info.Chat,
+		&waE2E.Message{
+			ExtendedTextMessage: &waE2E.ExtendedTextMessage{
+				Text: proto.String(message),
+				ContextInfo: &waE2E.ContextInfo{
+					StanzaID:      proto.String(e.Info.ID),
+					Participant:   proto.String(e.Info.Sender.ToNonAD().String()),
+					QuotedMessage: e.Message,
+				},
+			},
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (handler *Handler) GetTextMessage(filename string) (string, error) {
+	workdir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Open(path.Join(workdir, "messages", filename))
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	b, err := io.ReadAll(file)
+	return string(b), nil
+}
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"VilmanakaBot/handler"
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	startWhatsappBot()
+}
+
+func startWhatsappBot() {
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:database?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+
+	handl := handler.Handler{
+		Client: client,
+	}
+	client.AddEventHandler(handl.EventHandler)
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				// Render the QR code here
+				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
+				//fmt.Println("QR code:", evt.Code)
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
